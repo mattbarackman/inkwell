962fd37ff131abf834b571783ef3cf58759a56ff
Apprentice <apprentice@dbc24.local>
2013-07-03 16:04:46 -0700

    initial commit. added testing gems, heroku gem, and awesome_print. moved to postgres database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inkwell::Application.config.secret_token = '732174606d2374749bb9b193ecea8fdd6d7313eff3a72b35dd9a6b8b0d8acd86f934823d90dd7d727bbfed170717db435506d712d4f73f716e2ed617ed8d502f'
