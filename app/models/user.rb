6df8d6566e5e3a1bd14bdd98c99c36dec9f4c9f2
Nicholas Kirchner <kirc0076@umn.edu>
2013-07-04 12:01:57 -0700

    edited user forms to include name & address, badly

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me,
+                  :first_name, :last_name, :street_address, :city, :state,
+                  :zipcode
   # attr_accessible :title, :body
   has_many :friends
   has_many :occasions, :through => :friends
