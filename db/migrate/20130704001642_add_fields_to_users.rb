c4c56c2664b25ce64b013014fba7c416da9498de
Apprentice <apprentice@dbc19.local>
2013-07-03 17:24:51 -0700

    Added admins, imported user registration views from devise

---
+++ db/migrate/20130704001642_add_fields_to_users.rb
@@ -0,0 +1,10 @@
+class AddFieldsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :street_address, :string
+    add_column :users, :city, :string
+    add_column :users, :state, :string
+    add_column :users, :zipcode, :string
+  end
+end
