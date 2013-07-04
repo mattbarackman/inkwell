d8538967df347fea816ce44dc52b83a7a38d839c
Nicholas Kirchner <kirc0076@umn.edu>
2013-07-04 13:08:19 -0700

    Added update friends functionality, improved the users friends view

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,23 +1,35 @@
 class FriendsController < ApplicationController
 
-	before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
-	def index
-	end
+  def index
+  end
+  
+  def new
+    @friend = current_user.friends.new
+  end
 
-	def new
-		@friend = current_user.friends.new
-	end
+  def create
+    friend = current_user.friends.build
+    if friend.update_attributes(params[:friend])
+      redirect_to friends_path
+    else
+      redirect_to user_root_path
+    end
+  end
 
-	def create
-		friend = current_user.friends.build
-		if friend.update_attributes(params[:friend])
-			redirect_to '/friends'
-		else
-			redirect_to '/profile'
-		end
-	end
+  def edit
+    @friend = Friend.find(params[:id])
+    redirect_to user_root_path unless @friend.user == current_user
+  end
 
-
+  def update
+    friend = Friend.find(params[:id])
+    if friend.user == current_user && friend.update_attributes(params[:friend])
+      redirect_to friends_path
+    else
+      redirect_to user_root_path
+    end
+  end
 
 end
