6b50d0b8d55904349dd1541011291470d2848f51
mattbarackman <matthew.barackman@gmail.com>
2013-07-05 10:49:09 -0700

    removed FactoryGirl.find_definitions from rakefile

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-FactoryGirl.find_definitions
-
 # Initialize the rails application
 Inkwell::Application.initialize!
 
