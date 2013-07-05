f82095ef5aa61c6691f9a0fdbc4f214c42b71784
Nicholas Kirchner <kirc0076@umn.edu>
2013-07-04 19:12:30 -0700

    Rewrote try_to_update method to do error handling and to prevent unauthorized edits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,24 @@ class ApplicationController < ActionController::Base
 
   def try_to_update(resource, options = {})
     
-    redirect_to user_root_path unless resource
-    redirect_to user_root_path if options[:authorize_user] && resource.user != current_user
-
     resource_symbol = resource.class.to_s.downcase.to_sym
-    redirect_to user_root_path unless resource.update_attributes(params[resource_symbol])
-
     resource_path = eval "#{resource.class.to_s.downcase.pluralize}_path"
-    redirect_to resource_path
+
+    if !resource
+      redirect_to user_root_path, notice: "No resource", status: :unprocessable_entity
+    else
+
+      authorized = !options[:authorize_user] || resource.user == current_user
+
+      if !authorized
+        redirect_to user_root_path, notice: "Not authorized", status: :unauthorized
+      elsif resource.update_attributes(params[resource_symbol])
+        redirect_to resource_path
+      else
+        redirect_to user_root_path, notice: "Failed to save", status: :internal_server_error
+      end
+    end
+    
   end
+
 end
