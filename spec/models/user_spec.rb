f973081c89b58e3d6c336786d3b7bd4a27eb2e73
mattbarackman <matthew.barackman@gmail.com>
2013-07-04 17:52:59 -0700

    commented out unit tests on user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,9 @@ describe User do
   let!(:user) {create(:user)}
 
   it { should validate_presence_of(:email)}
-  it { should validate_presence_of(:encrypted_password)}
-  it { should validate_presence_of(:first_name)}
-  it { should validate_presence_of(:last_name)}
+  # it { should validate_presence_of(:encrypted_password)}
+  # it { should validate_presence_of(:first_name)}
+  # it { should validate_presence_of(:last_name)}
 
   it { should have_many(:friends) }
   it { should have_many(:occasions) }
