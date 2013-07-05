db3218949d7c583b38b9307e46c14c8267a120ef
mattbarackman <matthew.barackman@gmail.com>
2013-07-05 12:03:51 -0700

    moved Esty config lines in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -7,13 +7,11 @@ Inkwell::Application.initialize!
 require 'yaml'
 
 if ENV['RAILS_ENV'] == "development"
-
   config = YAML.load_file("#{Rails.root}/config/web_app_api.yaml")
-  Etsy.api_key = config['ETSY_KEYSTRING']
-  Etsy.api_secret = config['ETSY_SECRET']
-
 end
 
+Etsy.api_key = config['ETSY_KEYSTRING']
+Etsy.api_secret = config['ETSY_SECRET']
 Etsy.callback_url = 'http://localhost:3000'
 Etsy.environment = :production
 
