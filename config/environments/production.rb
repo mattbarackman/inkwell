93ff653aca25718d6705aeaf159a8cdd84b20dd0
mattbarackman <matthew.barackman@gmail.com>
2013-07-08 09:44:02 -0700

    added smtp method config for actionmailer in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,6 @@ Inkwell::Application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-
+  config.action_mailer.delivery_method = :smtp
 
 end
