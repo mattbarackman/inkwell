b46ead588f034f46cdf61406aeb491eb0ec2b66e
mattbarackman <matthew.barackman@gmail.com>
2013-07-07 17:45:34 -0700

    adjusted environment.rb file to load env variables of etsy api keys

--- config/environment.rb
+++ config/environment.rb
@@ -8,11 +8,17 @@ require 'yaml'
 
 unless Rails.env.production?
   config = YAML.load_file("#{Rails.root}/config/web_app_api.yaml")
+  Etsy.api_key = config['ETSY_KEYSTRING']
+  Etsy.api_secret = config['ETSY_SECRET']
 end
 
-Etsy.api_key = config['ETSY_KEYSTRING']
-Etsy.api_secret = config['ETSY_SECRET']
+if Rails.env.production?
+  Etsy.api_key = ENV['ETSY_KEYSTRING']
+  Etsy.api_secret = ENV['ETSY_SECRET']
+end
+
 Etsy.callback_url = 'http://localhost:3000'
 Etsy.environment = :production
+
 
 
