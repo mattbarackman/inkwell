a2262003bef62108ff8db4ddae9721f2126c1d92
Nicholas Kirchner <kirc0076@umn.edu>
2013-07-08 11:56:17 -0700

    added jquery-ui, started draggable/droppable logic

--- app/assets/javascripts/drag_cards.js
+++ app/assets/javascripts/drag_cards.js
@@ -1,25 +1,36 @@
-function Occasion() {}
-
-Occasion.prototype = {}
+//= require jquery.ui.draggable
+//= require jquery.ui.droppable
 
 function UpcomingQueue() {
     this.occasions = [];
 }
 
-UpcomingQueue.prototype = {}
+UpcomingQueue.prototype = {};
 
 function ShoppingCart() {
     this.occasions = [];
 }
 
-ShoppingCart.prototype = {}
+ShoppingCart.prototype = {};
 
-function getOccasions() {
-    $.get("/occasions/js", function(data) {
-        console.log(data);
-    });
-}
+cartRenderer = {};
+
+queueRenderer = {};
 
 $(document).ready(function() {
-    getOccasions();
+    var cart = new ShoppingCart();
+    var queue = new UpcomingQueue();
+    $.get("/occasions/js", function(data) {
+        cart.occasions = data.future;
+        queue.occasions = data.upcoming;
+    });
+
+    $( ".card img" ).draggable({ helper: "clone" });
+    $(".occasion_partial").droppable({
+        accept: ".card img",
+        drop: function(e, ui) {
+            console.log(e);
+            console.log(ui);
+        }
+    });
 })
