25ede60ed094fd7bf7eb7553ffd04ba0f8d005df
Apprentice <apprentice@dbc04.local>
2013-07-09 13:40:51 -0700

    fixed bug when importing facebook users. now it doesn't save the year

--- app/models/occasion.rb
+++ app/models/occasion.rb
@@ -25,12 +25,8 @@ class Occasion < ActiveRecord::Base
     /(\d{2})\/(\d{2})/.match(birthday)
     month = $1
     day = $2
-    
-    year = "2013"
-    year = $1 if /(\d{4})/.match(birthday)
-
-    date = "#{day}-#{month}-#{year}"      
-    DateTime.parse(date)
+    date = "#{day}-#{month}"      
+    Date.strptime(date, "%d-%m")
   end
   
   def create_order
