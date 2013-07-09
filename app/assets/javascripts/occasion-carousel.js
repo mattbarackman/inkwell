c84f5befea8871963be3c5178a7176bf494a5dd7
dbc-apprentice <dbc@dbc.com>
2013-07-09 14:01:06 -0700

    added carousel for occasions in sidebar

---
+++ app/assets/javascripts/occasion-carousel.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $('.jcarousel_test').jcarousel({
+      'list': '.pending_orders',
+      'items': '.occasion_partial',
+      'vertical': true,
+      'wrap': 'circular'
+  });
+
+  $('.scrollup').on('click', function(e){
+    e.preventDefault();
+    $('.jcarousel_test').jcarousel('scroll', '-=2');
+  });
+
+  $('.scrolldown').on('click', function(e){
+    e.preventDefault();
+    $('.jcarousel_test').jcarousel('scroll', '+=2');
+  });
+});
