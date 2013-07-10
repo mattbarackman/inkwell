ac045923f07dfdfc59ff09d30fe3d9730a8fa180
dbc-apprentice <dbc@dbc.com>
2013-07-10 16:51:50 -0700

    fixed photos and made it so you can't create a duplicate birthday when importing bday from facebook

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -8,16 +8,19 @@ class Friend < ActiveRecord::Base
   
 
   def self.add_fb_friend(current_user, params)
-    friend = Friend.find_or_create_by_name_and_user_id(params[:name], current_user.id)
-    friend.image_url = params[:image_url]
+    existing_friend = Friend.find_by_name_and_user_id(params[:name], current_user.id)
     
-    if friend.save && params[:birthday].length > 1
-      occasion = friend.occasions.build
-      occasion.date = Occasion.parse_birthday(params[:birthday])
-      occasion.name = "#{friend.name}'s Birthday!"
-      occasion.event_type_name = 'birthday'
-      occasion.annual = true
-      occasion.save
+    unless existing_friend
+      friend = Friend.create(name: params[:name], user_id: current_user.id)
+      friend.image_url = params[:image_url]
+      if friend.save && params[:birthday].length > 1
+        occasion = friend.occasions.build
+        occasion.date = Occasion.parse_birthday(params[:birthday])
+        occasion.name = "#{friend.name}'s Birthday!"
+        occasion.event_type_name = 'birthday'
+        occasion.annual = true
+        occasion.save
+      end
     end
   end
 end
