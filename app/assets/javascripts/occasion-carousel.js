da1dcbf567383285c8b6938cfabcd208bee84ea8
dbc-apprentice <dbc@dbc.com>
2013-07-09 22:29:43 -0700

    finished adding cards to occasions repeatedly. modified sizes of images

--- app/assets/javascripts/occasion-carousel.js
+++ app/assets/javascripts/occasion-carousel.js
@@ -6,13 +6,13 @@ $(document).ready(function(){
       'wrap': 'circular'
   });
 
-  $('.scrollup').on('click', function(e){
+  $('.scrollup').on('mouseover', function(e){
     e.preventDefault();
-    $('.jcarousel_test').jcarousel('scroll', '-=2');
+    $('.jcarousel_test').jcarousel('scroll', '-=1');
   });
 
-  $('.scrolldown').on('click', function(e){
+  $('.scrolldown').on('mouseover', function(e){
     e.preventDefault();
-    $('.jcarousel_test').jcarousel('scroll', '+=2');
+    $('.jcarousel_test').jcarousel('scroll', '+=1');
   });
 });
